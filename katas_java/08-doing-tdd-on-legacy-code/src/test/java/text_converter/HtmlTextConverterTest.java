649f4099608627eb4e7a7eafdc5d497765dd1bc4
mjtordesillas <you@example.com>
2019-06-13 11:36:39 +0200

    moved instantiation of HTMLTextConverter to SetUp

--- katas_java/08-doing-tdd-on-legacy-code/src/test/java/text_converter/HtmlTextConverterTest.java
+++ katas_java/08-doing-tdd-on-legacy-code/src/test/java/text_converter/HtmlTextConverterTest.java
@@ -1,20 +1,49 @@
 package text_converter;
 
+import org.junit.Before;
 import org.junit.Test;
 import java.io.IOException;
 import static org.junit.Assert.*;
 
 public class HtmlTextConverterTest {
 
+    private TestingHtmlTextConverter converter;
+
+    @Before
+    public void setUp() throws Exception {
+        converter = new TestingHtmlTextConverter("any path");
+    }
+
     @Test
     public void converts_ampersand_symbol() throws IOException
     {
-        TestingHtmlTextConverter converter = new TestingHtmlTextConverter("any path");
         converter.lineToReturn = "&";
 
         converter.convertToHtml("any output file path");
 
         assertTrue(converter.stringsWritten.contains("&amp;"));
         assertFalse(converter.stringsWritten.contains("&"));
+    }
+
+    @Test
+    public void converts_less_than_symbol() throws IOException
+    {
+        converter.lineToReturn = "<";
+
+        converter.convertToHtml("any output file path");
+
+        assertTrue(converter.stringsWritten.contains("&lt;"));
+        assertFalse(converter.stringsWritten.contains("<"));
+    }
+
+    @Test
+    public void converts_greater_than_symbol() throws IOException
+    {
+        converter.lineToReturn = ">";
+
+        converter.convertToHtml("any output file path");
+
+        assertTrue(converter.stringsWritten.contains("&gt;"));
+        assertFalse(converter.stringsWritten.contains(">"));
     }
 }
